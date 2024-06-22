bf22bf8a0f3e817413a34a0a901a0bc80021fd32
Satya prakash <satyapprakash98@gmail.com>
2024-06-22 19:55:42 +0530

    car added

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
         Car car1 = new Car("C001", "Toyota", "Camry", 50);
         Car car2 = new Car("C002", "Hyundai", "Verna", 60);
         Car car3 = new Car("C003", "Mahindra", "Thar", 70);
+        Car car4 = new Car("C004", "Mahindra", "Nexon", 55);
         CarRentalSystem rentalSystem = new CarRentalSystem();
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
+        rentalSystem.addCar(car4);
         rentalSystem.menu();
     }
 }
